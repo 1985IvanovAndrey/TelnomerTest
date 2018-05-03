86e5dafcc255aab4baeea7ae57df696a171815f5
1985IvanovAndrey <dron_krotova@i.ua>
2018-05-03 23:11:04 +0300

    Initial commit

---
+++ src/main/java/Runner.java
@@ -0,0 +1,6 @@
+public class Runner {
+    public static void main(String[] args) {
+       // CheckTelNomer checkTelNomer=new CheckTelNomer();
+        //System.out.println(checkTelNomer.checkNum1("+142  54782592"));
+    }
+}
